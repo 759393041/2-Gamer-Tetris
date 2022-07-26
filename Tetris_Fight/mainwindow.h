7f60cff05b8854d16fc280f8e49e23f14621701c
759393041 <81805379+759393041@users.noreply.github.com>
2022-07-26 17:11:38 +0800

    1

--- Tetris_Fight/mainwindow.h
+++ Tetris_Fight/mainwindow.h
@@ -25,17 +25,17 @@ public:
     void paintEvent(QPaintEvent *) override;
     void keyReleaseEvent(QKeyEvent * event) override;
     void keyPressEvent(QKeyEvent * event) override;
-
+    void timerEvent(QTimerEvent *event)override;
 
 public slots:
     void start_game();
-    void timer_start();
     void new_game();
 
 private:
     Ui::MainWindow *ui;
     gameEngine* ge;
-    QTimer* timer;
+    int timer_pressdown_id;
+    int timer_releaseup_id;
     bool game_is_started;
 
 };
